06838737fc9b049e6e25ce59eeeb81541e84bebe
ashleymills <ash730@gmail.com>
2021-02-03 09:59:35 -0500

    add/removed print statments commit 5/5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,13 @@ public class Main {
         // (1)  two commits = more than one line of changes
         // (1)  add/remove an existing file
 
-        System.out.println("Hello, commit two of five"); //(2)
+        //System.out.println("Hello, commit two of five"); //(2)
         // (2) Line two of change for second commit
+
+        //Final Commit
+        System.out.println("Hello, Final Commit (5/5). Have a nice day :)");
+        //Random print statment
+        System.out.print('5' + '/' + '5');
+
     }
 }
