e526839c3b92e5d0627d3f3c45fd3b669cdb03b3
ashleymills <ash730@gmail.com>
2021-02-03 09:35:31 -0500

    Two comments on what to do for part two of assignment 2 (2/5) -- Changes -- Println statement of what commit it was, second comment of the change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    // two commits = more than one line of changes
-        // add/remove an existing file
+        // (1)  two commits = more than one line of changes
+        // (1)  add/remove an existing file
+
+        System.out.println("Hello, commit two of five"); //(2)
+        // (2) Line two of change for second commit
     }
 }
